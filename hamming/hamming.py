6e98d23126079b3b44ae79a4b66d20c6e0f16d54
haro-freezd <harmanmahal@gmail.com>
2018-06-02 01:53:43 -0700

    Hamming distance problem

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+  if(len(strand_a) != len(strand_b)):
+    raise ValueError('Different sized values')
+  hamming_dist = 0
+  for i, j in zip(strand_a, strand_b):
+    if i != j:
+      hamming_dist += 1
+  return hamming_dist
